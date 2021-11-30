2cc615a89306afe93c315ef627c5b4b1de4bba4b
Kazuya-san <kazuyasan3286@gmail.com>
2021-11-30 22:10:34 +0500

    v3

--- server.js
+++ server.js
@@ -1,19 +1,23 @@
 const express = require("express");
 const app = express();
 const http = require("http");
+const path = require("path");
 const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
-
-app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/public/index.html");
-});
 
 io.on("connection", (socket) => {
   console.log("New user connected");
   socket.on("chat message", (msg) => {
     socket.broadcast.emit("chat message", msg);
   });
+});
+
+const paths = __dirname + "/public/";
+app.use(express.static(paths));
+
+app.get("/", function (req, res) {
+  res.sendFile(paths + "index.html");
 });
 
 const PORT = process.env.PORT || 3000;
