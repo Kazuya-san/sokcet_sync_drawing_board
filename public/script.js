7b1d0ddc1a93f4c9f635e2fd31118212ef2de826
Kazuya-san <kazuyasan3286@gmail.com>
2022-09-28 13:25:12 +0500

    added support for multiple rooms

--- public/script.js
+++ public/script.js
@@ -1,45 +1,48 @@
 let currentColor = "black";
 let socket = io();
 let bg = 220;
+let id;
 
 function setup() {
   //create full size canvas
   createCanvas(innerWidth, 600);
   background(bg);
   strokeWeight(5);
+  id = new URL(window.location.href).pathname.substring(7);
+  socket.emit("join-room", id);
 }
 
 document.getElementById("clearcan").addEventListener("click", function () {
   background(bg);
-  socket.emit("chat message", {
+  socket.emit("chat message", id, {
     message: "clear",
   });
 });
 
 document.getElementById("redcl").addEventListener("click", function () {
   currentColor = "red";
-  socket.emit("chat message", {
+  socket.emit("chat message", id, {
     message: "red",
   });
 });
 
 document.getElementById("greencl").addEventListener("click", function () {
   currentColor = "green";
-  socket.emit("chat message", {
+  socket.emit("chat message", id, {
     message: "green",
   });
 });
 
 document.getElementById("bluecl").addEventListener("click", function () {
   currentColor = "blue";
-  socket.emit("chat message", {
+  socket.emit("chat message", id, {
     message: "blue",
   });
 });
 
 document.getElementById("blackcl").addEventListener("click", function () {
   currentColor = "black";
-  socket.emit("chat message", {
+  socket.emit("chat message", id, {
     message: "black",
   });
 });
@@ -49,7 +52,7 @@ function draw() {
   if (mouseIsPressed == true) {
     stroke(currentColor);
     line(mouseX, mouseY, pmouseX, pmouseY);
-    socket.emit("chat message", {
+    socket.emit("chat message", id, {
       mouseX: mouseX,
       mouseY: mouseY,
       pmouseX: pmouseX,
--- server.js
+++ server.js
@@ -5,23 +5,49 @@ const path = require("path");
 const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
+const uuid = require("uuid");
+
+// io.on("connection", (socket) => {
+//   console.log("New user connected");
+//   socket.on("chat message", (msg) => {
+//     socket.broadcast.emit("chat message", msg);
+//   });
+
+//   socket.on("disconnect", () => {
+//     console.log("User disconnected");
+//   });
+// });
 
 io.on("connection", (socket) => {
   console.log("New user connected");
-  socket.on("chat message", (msg) => {
-    socket.broadcast.emit("chat message", msg);
+
+  //listen for join-room event
+  socket.on("join-room", (roomId) => {
+    //console.log(roomId, userId);
+    socket.join(roomId);
+    //io.to(roomId).emit("user-connected", userId);
+  });
+
+  socket.on("chat message", (roomId, msg) => {
+    io.to(roomId).emit("chat message", msg);
   });
 
   socket.on("disconnect", () => {
     console.log("User disconnected");
+    console.log(io.sockets.adapter.rooms);
   });
 });
 
 const paths = __dirname + "/public/";
-app.use(express.static(paths));
+app.use(express.static(paths + "/script.js"));
 
 app.get("/", function (req, res) {
-  res.sendFile(path.join(paths, "index.html"));
+  console.log("redirecting");
+  return res.redirect(`/board/${uuid.v4()}`);
+});
+
+app.get("/board/:id", function (req, res) {
+  res.sendFile(path.join(paths, "/index.html"));
 });
 
 const PORT = process.env.PORT || 5000;
